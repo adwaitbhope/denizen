2cbb235a86e4dc583c37268b0b8a163dc0949532
aashayzanpure <aashayz28@gmail.com>
2019-09-07 14:30:45 +0530

    Updated Login Screen with 'Register Society' DialogFragment created.

--- app/src/main/java/com/township/manager/LoginScreenActivity.java
+++ app/src/main/java/com/township/manager/LoginScreenActivity.java
@@ -1,28 +1,51 @@
 package com.township.manager;
 
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class LoginScreenActivity extends AppCompatActivity {
-    private Button button;
+public class LoginScreenActivity extends FragmentActivity {
+    private Button button4,button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
 
-        button = (Button) findViewById(R.id.button4);
-        button.setOnClickListener(new View.OnClickListener() {
+        button4 = findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openDialog();
+            }
+        });
+        button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialogFragment();
+
             }
         });
     }
     public void openDialog() {
         ExampleDialog exampleDialog = new ExampleDialog();
         exampleDialog.show(getSupportFragmentManager(), "example dialog");
+    }
+
+    public void openDialogFragment(){
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        Fragment prev = getSupportFragmentManager().findFragmentByTag("dialog");
+        if (prev != null) {
+            ft.remove(prev);
+        }
+        ft.addToBackStack(null);
+        DialogFragment dialogFragment = new RegistrationDialogFragment();
+        dialogFragment.show(ft, getString(R.string.dialog));
     }
 }
