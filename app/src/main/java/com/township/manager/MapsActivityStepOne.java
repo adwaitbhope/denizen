18350e084b5f7bf39489966cdd270f29f651d908
tacitonic <tacitonic@gmail.com>
2019-09-20 15:45:07 +0530

    Completed Maps Activity for registration

--- app/src/main/java/com/township/manager/MapsActivityStepOne.java
+++ app/src/main/java/com/township/manager/MapsActivityStepOne.java
@@ -1,28 +1,76 @@
 package com.township.manager;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
-import android.os.Bundle;
-
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MapsActivityStepOne extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Location Last_Known_Location;
 
-    @Override
+    private final LatLng mDefaultLocation = new LatLng(-33.8523341, 151.2106085);
+    private static final int DEFAULT_ZOOM = 15;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private boolean mLocationPermissionGranted = false;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private boolean marker_set;
+    Button confirnButton;
+    LatLng LatLong;
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps_step_one);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        confirnButton = (Button) findViewById(R.id.map_confirm_location_button);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        confirnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (LatLong == null) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MapsActivityStepOne.this);
+                    builder.setTitle("No location selected")
+                            .setMessage("Please tap on the desired location and then proceed.")
+                            .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                }
+                            });
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                } else {
+                    Intent intent = new Intent();
+                    intent.putExtra("LatLong", LatLong);
+                    setResult(RESULT_OK, intent);
+                    finish();
+                }
+            }
+        });
     }
 
 
@@ -38,10 +86,124 @@ public class MapsActivityStepOne extends FragmentActivity implements OnMapReadyC
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        getDeviceLocation();
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(18.4915, 73.8217);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker in Pune and move the camera
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+
+
+            @Override
+            public void onMapClick(LatLng latLng) {
+                mMap.clear();
+                mMap.addMarker(new MarkerOptions().position(latLng).title("Marker in Pune"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                marker_set = true;
+                LatLong = latLng;
+            }
+        });
+
     }
+
+    private void getDeviceLocation() {
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+//        Toast.makeText(getContext(), "get device location", Toast.LENGTH_SHORT).show();
+
+        try {
+            if (mLocationPermissionGranted) {
+                Task<Location> locationResult = mFusedLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(this, new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        LatLng currentLocation = new LatLng(Last_Known_Location.getLatitude(), Last_Known_Location.getLongitude());
+                        if (task.isSuccessful()) {
+                            // Set the map's camera position to the current location of the device.
+                            Last_Known_Location = task.getResult();
+                            currentLocation = new LatLng(Last_Known_Location.getLatitude(), Last_Known_Location.getLongitude());
+                            mMap.clear();
+                            mMap.addMarker(new MarkerOptions().position(currentLocation).title("Marker in Pune"));
+                            mMap.moveCamera(CameraUpdateFactory
+                                    .newLatLngZoom(currentLocation, DEFAULT_ZOOM));
+                            mMap.setMyLocationEnabled(true);
+                        } else {
+                            mMap.clear();
+                            mMap.addMarker(new MarkerOptions().position(currentLocation).title("Marker in Pune"));
+//                            mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
+                            mMap.moveCamera(CameraUpdateFactory
+                                    .newLatLngZoom(mDefaultLocation, DEFAULT_ZOOM));
+                            mMap.getUiSettings().setMyLocationButtonEnabled(true);
+                        }
+                    }
+                });
+            } else {
+                getLocationPermission();
+            }
+        } catch (SecurityException e) {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
+    private void getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
+//            Toast.makeText(getContext(), "getLocationPermission:mLocationPermissionGranted:-"+mLocationPermissionGranted, Toast.LENGTH_SHORT).show();
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+//            Toast.makeText(getContext(), "getLocationPermission:mLocationPermissionGranted:-"+mLocationPermissionGranted, Toast.LENGTH_SHORT).show();
+        }
+        updateLocationUI();
+//        onRequestPermissionsResult(PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION,android.Manifest.permission.ACCESS_FINE_LOCATION,PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+    }
+
+    /**
+     * Handles the result of the request for location permissions.
+     */
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        mLocationPermissionGranted = false;
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mLocationPermissionGranted = true;
+//                    Toast.makeText(getContext(), "onRequestPermissionsResult:-", Toast.LENGTH_SHORT).show();
+                }
+            }
+//            Toast.makeText(getContext(), "onRequestPermissionsResult:-"+mLocationPermissionGranted, Toast.LENGTH_SHORT).show();
+        }
+        updateLocationUI();
+    }
+
+    private void updateLocationUI() {
+        if (mMap == null) {
+            return;
+        }
+        try {
+            if (mLocationPermissionGranted) {
+                mMap.setMyLocationEnabled(true);
+                mMap.getUiSettings().setMyLocationButtonEnabled(true);
+            } else {
+                mMap.setMyLocationEnabled(false);
+                mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                Last_Known_Location = null;
+                getLocationPermission();
+            }
+        } catch (SecurityException e) {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
 }
--- app/src/main/java/com/township/manager/RegistrationStepOne.java
+++ app/src/main/java/com/township/manager/RegistrationStepOne.java
@@ -1,38 +1,82 @@
 package com.township.manager;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class RegistrationStepOne extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private static int LOCATION_REQUEST_CODE = 1;
+    private static final int DEFAULT_ZOOM = 15;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_step_one);
-
+        Button setLocationButton = findViewById(R.id.registration_set_location_button);
+        setLocationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegistrationStepOne.this, MapsActivityStepOne.class);
+//                startActivity(intent);
+                startActivityForResult(intent, LOCATION_REQUEST_CODE);
+            }
+        });
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_embedded_map);
         mapFragment.getMapAsync(this);
 
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == LOCATION_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Bundle extras = data.getExtras();
+                if (extras != null) {
+                    LatLng currentLocation = data.getExtras().getParcelable("LatLong");
+                    mMap.clear();
+                    mMap.addMarker(new MarkerOptions().position(currentLocation));
+                    mMap.moveCamera(CameraUpdateFactory
+                            .newLatLngZoom(currentLocation, DEFAULT_ZOOM));
+                }
+            }
+        }
+    }
+
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        {
+            mMap.setOnCameraMoveListener(new GoogleMap.OnCameraMoveListener() {
+                @Override
+                public void onCameraMove() {
+//                    (MapsActivityStepOne).getDeviceLocation();
+                }
+            });
+        }
+
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(18.4915, 73.8217);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng pune = new LatLng(18.4915, 73.8217);
+        mMap.addMarker(new MarkerOptions().position(pune).title("Marker in Pune"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(pune));
     }
+
+
 }
