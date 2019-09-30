5ae006135f3a4734e0039b0b67dc29106bc4b09c
aashayzanpure <aashayz28@gmail.com>
2019-09-30 14:59:05 +0530

    AddSecurityActivity created.

---
+++ app/src/main/java/com/township/manager/AddSecurityActivity.java
@@ -0,0 +1,14 @@
+package com.township.manager;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddSecurityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_security);
+    }
+}
