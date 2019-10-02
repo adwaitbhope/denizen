4fd8f974c8a46e79f929234a710229b346ebd431
tacitonic <tacitonic@gmail.com>
2019-10-02 20:51:19 +0530

    Redesigned Register Complaint Activity, added toolbar. Changes to Complaints list.

--- app/src/main/java/com/township/manager/ComplaintsListFragment.java
+++ app/src/main/java/com/township/manager/ComplaintsListFragment.java
@@ -52,9 +52,12 @@ public class ComplaintsListFragment extends Fragment {
         return fragment;
     }
 
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
--- app/src/main/java/com/township/manager/RegisterComplaintActivity.java
+++ app/src/main/java/com/township/manager/RegisterComplaintActivity.java
@@ -3,6 +3,7 @@ package com.township.manager;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class RegisterComplaintActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class RegisterComplaintActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_complaint);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.register_complaint_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Register Complaint");
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
