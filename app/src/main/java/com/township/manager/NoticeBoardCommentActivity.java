9ad027872cc2dc68fb474a6c48d34cc48f7c4b0a
tacitonic <tacitonic@gmail.com>
2019-10-05 14:16:28 +0530

    Added comment activity for notice board.

---
+++ app/src/main/java/com/township/manager/NoticeBoardCommentActivity.java
@@ -0,0 +1,14 @@
+package com.township.manager;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NoticeBoardCommentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notice_board_comment);
+    }
+}
