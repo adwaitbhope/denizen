4868e1f3bc5c5c9497a2b7888cd25a4191b22a5f
tacitonic <tacitonic@gmail.com>
2019-10-24 20:24:27 +0530

    Connected Membership Details exFAB and minor changes.

--- app/src/main/java/com/township/manager/AmenitiesFragment.java
+++ app/src/main/java/com/township/manager/AmenitiesFragment.java
@@ -76,13 +76,14 @@ public class AmenitiesFragment extends Fragment implements View.OnClickListener 
         ExtendedFloatingActionButton membershipDetailsButton = view.findViewById(R.id.membership_details_ex_fab);
         membershipDetailsButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
+
                 Intent intent = new Intent(getContext(), MembershipDetailsActivity.class);
                 startActivity(intent);
             }
         });
 
-        return inflater.inflate(R.layout.fragment_amenities, container, false);
+        return(view);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
--- app/src/main/java/com/township/manager/MembershipDetailsActivity.java
+++ app/src/main/java/com/township/manager/MembershipDetailsActivity.java
@@ -1,8 +1,10 @@
 package com.township.manager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MembershipDetailsActivity extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class MembershipDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_membership_details);
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.membership_details_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Membership Details");
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
