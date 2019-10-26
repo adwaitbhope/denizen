22ff247392f5557a2a68167c5ce2c967a1682c7d
tacitonic <tacitonic@gmail.com>
2019-10-26 17:35:01 +0530

    Added amenity details activity.

---
+++ app/src/main/java/com/township/manager/AmenityDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.township.manager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AmenityDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_amenity_details);
+    }
+}
