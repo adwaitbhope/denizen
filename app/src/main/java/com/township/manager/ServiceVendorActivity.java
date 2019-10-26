5667bc2813dab84c82bd1577f42a16062d2c10bc
aashayzanpure <aashayz28@gmail.com>
2019-10-26 19:46:02 +0530

    Service Vendor activity made with improvised cardview.

--- app/src/main/java/com/township/manager/ServiceVendorActivity.java
+++ app/src/main/java/com/township/manager/ServiceVendorActivity.java
@@ -2,7 +2,11 @@ package com.township.manager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ServiceVendorActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class ServiceVendorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service_vendor);
+
+        FloatingActionButton fab = findViewById(R.id.add_vendor_floatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ServiceVendorActivity.this, AddServiceVendorActivity.class));
+
+
+            }
+        });
+
     }
 }
