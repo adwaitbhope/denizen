2b1f5165a52c450f24d1e70406433775c00fe5fe
tacitonic <tacitonic@gmail.com>
2019-10-26 19:45:47 +0530

    Added one time fee details list activity.

---
+++ app/src/main/java/com/township/manager/OneTimeFeeDetailsListActivity.java
@@ -0,0 +1,14 @@
+package com.township.manager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OneTimeFeeDetailsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one_time_fee_details_list);
+    }
+}
