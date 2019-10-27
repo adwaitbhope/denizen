b1da9e4997dd5af4e8ab11aa9b1d92efd519b54c
aashayzanpure <aashayz28@gmail.com>
2019-10-27 17:12:16 +0530

    TimePicker for AddSecurity Implemented.

--- app/src/main/java/com/township/manager/AddSecurityActivity.java
+++ app/src/main/java/com/township/manager/AddSecurityActivity.java
@@ -1,27 +1,47 @@
 package com.township.manager;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
-public class AddSecurityActivity extends AppCompatActivity{
+public class AddSecurityActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_security);
 
-//        String[] COMPLAINT_FILTER = new String[]{"Gate 1", "Gate 2", "Gate 2"};
-//
-//        ArrayAdapter<String> adapter =
-//                new ArrayAdapter<>(
-//                        this,
-//                        R.layout.dropdown_menu_popup_item,
-//                        COMPLAINT_FILTER);
-//
-//        AutoCompleteTextView editTextFilledExposedDropdown = findViewById(R.id.gate_number_textinput_child);
-//        editTextFilledExposedDropdown.setAdapter(adapter);
+        Button from_button = findViewById(R.id.add_security_personnel_shift_from_button);
+        Button till_button = findViewById(R.id.add_security_personnel_shift_till_button);
+        from_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment timePicker = new TimePickerClass();
+                timePicker.show(getSupportFragmentManager(), "time picker");
+            }
+        });
+        till_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment timePicker = new TimePickerClass();
+                timePicker.show(getSupportFragmentManager(), "time picker");
+            }
+        });
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+//        TextView textView = (TextView) findViewById(R.id.textView);
+//        textView.setText("Hour: " + hourOfDay + " Minute: " + minute);
+        Toast.makeText(this, "Disla ka?", Toast.LENGTH_SHORT).show();
     }
 }
