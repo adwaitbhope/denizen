aeb0738a7199830af026c49c2c3a10ce36f843b3
Adwait Bhope <adwaitbhope@gmail.com>
2019-10-27 10:37:41 +0530

    Bug fixes

--- app/src/main/java/com/township/manager/SecurityActivity.java
+++ app/src/main/java/com/township/manager/SecurityActivity.java
@@ -1,11 +1,15 @@
 package com.township.manager;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -13,33 +17,40 @@ import com.google.android.material.tabs.TabLayout;
 
 import java.util.Objects;
 
-public class SecurityActivity extends AppCompatActivity implements ComplaintsListFragment.OnFragmentInteractionListener {
+public class SecurityActivity extends AppCompatActivity implements SecurityDesksListFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_security);
-        FloatingActionButton button = findViewById(R.id.add_new_security);
         SliderAdapter sliderAdapter = new SliderAdapter(Objects.requireNonNull(this).getSupportFragmentManager());
 
-        // TODO: Change the fragments that are loading here
-        sliderAdapter.addFragment(new ComplaintsListFragment(), "Pending");
-        sliderAdapter.addFragment(new ComplaintsListFragment(), "Resolved");
+        sliderAdapter.addFragment(new SecurityDesksListFragment(), "Personnel");
+        sliderAdapter.addFragment(new SecurityDesksListFragment(), "Desks");
 
         ViewPager mSlideViewPager = (ViewPager) findViewById(R.id.security_view_pager);
         mSlideViewPager.setAdapter(sliderAdapter);
         mSlideViewPager.setOffscreenPageLimit(1);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.security_info_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Security");
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         TabLayout tabLayout = (TabLayout) findViewById(R.id.security_tab_layout);
         tabLayout.setupWithViewPager(mSlideViewPager);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(SecurityActivity.this, RegisterComplaintActivity.class);
-                startActivity(intent);
-            }
-        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
