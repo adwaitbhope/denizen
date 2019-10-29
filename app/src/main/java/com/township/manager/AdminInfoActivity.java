6bbd4904691f891579bc93ca73fa0eb823103e62
aashayzanpure <aashayz28@gmail.com>
2019-10-30 00:52:13 +0530

    Extendable FABs added in finances section.

--- app/src/main/java/com/township/manager/AdminInfoActivity.java
+++ app/src/main/java/com/township/manager/AdminInfoActivity.java
@@ -2,13 +2,29 @@ package com.township.manager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class AdminInfoActivity extends AppCompatActivity {
 
+    FloatingActionButton floatingActionButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_info);
+        floatingActionButton = findViewById(R.id.add_admin_floatingActionButton);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AdminInfoActivity.this,AddAdminActivity.class));
+            }
+        });
     }
 }
