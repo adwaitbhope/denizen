ebbc569850379ebd1731c1c144522ac43595c66e
tacitonic <tacitonic@gmail.com>
2019-10-30 14:14:54 +0530

    Changes to servicevendors ui.

--- app/src/main/java/com/township/manager/AddServiceVendorActivity.java
+++ app/src/main/java/com/township/manager/AddServiceVendorActivity.java
@@ -2,6 +2,7 @@ package com.township.manager;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.room.Room;
@@ -101,6 +102,13 @@ public class AddServiceVendorActivity extends AppCompatActivity {
         password = cursor.getString(cursor.getColumnIndexOrThrow("Password"));
         township_id = cursor.getString(cursor.getColumnIndexOrThrow("TownshipId"));
 
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.add_service_vendor_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Add Vendor");
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         intent=getIntent();
         if(intent.getStringExtra("request").equals("1")) {
